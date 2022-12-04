f677416ac0652fe2ac34a16684ffd31d257df847
Charles A. Hartman <charles@hartman.dev>
2022-12-03 22:14:17 -0800

    day3 solutions

---
+++ day3/pt1_rucksack.py
@@ -0,0 +1,80 @@
+# pt1_rucksack.py -- adventOfCode2022 - day3 - part1
+
+"""
+algorithm
+- (main)
+    - declare (LETTERS) 'a..zA..Z'
+    - build (letterValue) - dictionary a..z + A..Z keys and appropriate values
+    - read input and assign to (items) list
+    - declare (score) to 0
+    - iterate through (items)
+        - call item processor function on each item and assign return to (halves)
+        - assign (Letter_values) to a..zA..Z 0 value dictionary
+        - iterate through halves[1] str
+            - if the letter is not in (halve[0])
+                - add that (letter_value) to (score)
+                - break
+    - return (score)
+
+
+- (itemProcessor) takes str and returns list with two strings
+    - strip \n from string
+    - get midpoint index of string
+    - return list of two elements, first_half and second_half
+
+
+- overkill and not necessary here, but saving from first pass:
+```
+import collections
+compartment = collections.Counter(halves[0])
+print(compartment)
+for letter in halves[1]:
+    if compartment[letter] != 0:
+        score += letter_values[letter]
+        sanity_check.append(letter)
+        continue
+```
+"""
+
+LETTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+
+def buildLetterValues(str: str, start: int) -> dict:
+    letter_values = {}
+    count = start
+    for l in str:
+        letter_values[l] = count
+        count += 1
+
+    return letter_values
+
+
+def itemProcessor(line: str) -> list:
+    line = line.strip()
+    mid = int(len(line) / 2)
+
+    return [line[:mid], line[mid:]]
+
+
+def processInput(filename: str) -> list:
+    input = open(filename, "r")
+
+    return input.readlines()
+
+
+def rucksackScore(inputFile: str) -> int:
+    letter_values = buildLetterValues(LETTERS, 1)
+    items = processInput(inputFile)
+    score = 0
+
+    for item in items:
+        halves = itemProcessor(item)
+        for letter in halves[1]:
+            if letter in halves[0]:
+                score += letter_values[letter]
+                break
+
+    return score
+
+
+print(rucksackScore("rucksack_input.txt"))
---
+++ day3/pt2_rucksack.py
@@ -0,0 +1,81 @@
+# pt2_rucksack.py -- adventOfCode2022 - day3 - part2
+
+"""
+algorithm
+- (main)
+    - declare (LETTERS) 'a..zA..Z'
+    - build (letterValue) - dictionary a..z + A..Z keys and appropriate values
+    - read input and assign to (lines) list
+    - break into 3 element subarrays [write (makeGroups) function]
+    - declare (score) to 0
+    - iterate through groups
+        - call (unique) on first element to eliminate duplicates, assign
+          to (keychain)
+        - iterate through keychain
+            - if letter is present in group[1] and group[2]
+                - add that letter_value to score
+        
+    - return (score)
+
+
+- (unique) takes str\n and returns string with duplicate letters removed
+    - strip \n from string
+    - build (unique_letters) without duplicate letters
+    - return (unique_letters)
+
+- (makeGroups) 
+    - take master input list and slices into subarrays of given length
+    - return array of subarrays
+"""
+
+LETTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+
+def buildLetterValues(str: str, start: int) -> dict:
+    letter_values = {}
+    count = start
+    for l in str:
+        letter_values[l] = count
+        count += 1
+
+    return letter_values
+
+
+def unique(line: str) -> str:
+    line = line.strip()
+    unique_letters = ""
+    for letter in line:
+        if letter not in unique_letters:
+            unique_letters += letter
+
+    return unique_letters
+
+
+def processInput(filename: str) -> list:
+    input = open(filename, "r")
+
+    return input.readlines()
+
+
+def makeGroups(lines: list, step: int) -> list:
+    groups = [lines[i : i + step] for i in range(0, len(lines), step)]
+
+    return groups
+
+
+def rucksackScore(inputFile: str) -> int:
+    letter_values = buildLetterValues(LETTERS, 1)
+    lines = processInput(inputFile)
+    groups = makeGroups(lines, 3)
+    score = 0
+
+    for group in groups:
+        keystring = unique(group[0])
+        for letter in keystring:
+            if letter in group[1] and letter in group[2]:
+                score += letter_values[letter]
+
+    return score
+
+
+print(rucksackScore("rucksack_input.txt"))
